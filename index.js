c4e577f6054c7724e11ff1af60b5d8ea43911131
Grace-3908254 <choore.11@gmail.com>
2024-10-23 15:57:52 -0400

    set up connection to public folder for images

---
+++ index.js
@@ -0,0 +1,33 @@
+// src/app.js
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+// import warehousesRoutes from './routes/warehouses-routes.js';
+// import inventoriesRoutes from './routes/inventories-routes.js'
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// Warehouses Routes
+// app.use('/warehouses', warehousesRoutes);
+// app.use('/inventories', inventoriesRoutes);
+
+// Basic API route
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
+
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log('Press CTRL + C to stop server');
+});
